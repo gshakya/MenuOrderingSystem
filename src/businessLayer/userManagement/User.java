27821fc58355b3a2289b21ee10fa30eb88b93ad6
Gunjan Shakya <gr.skya@gmail.com>
2015-12-11 15:04:21 -0600

    DB Connection layer refined

--- src/businessLayer/userManagement/User.java
+++ src/businessLayer/userManagement/User.java
@@ -1,6 +1,11 @@
 package businessLayer.userManagement;
 
 public class User {
+	@Override
+	public String toString() {
+		return "User [ id = " + id + ", firstName = " + firstName + ", lastName = " + lastName + "]";
+	}
+
 	/**
 	 * Class for the user
 	 */
@@ -8,42 +13,49 @@ public class User {
 	private String password;
 	private int id;
 	private String lastName;
-	
-	
 
-	public User(){
-		
+	public User() {
+
 	}
-	public User(int id ,String password,  String firstName, String lastName) {
+
+	public User(int id, String password, String firstName, String lastName) {
 		super();
 		this.firstName = firstName;
 		this.password = password;
 		this.id = id;
 		this.lastName = lastName;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String user) {
 		this.firstName = user;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String address) {
 		this.lastName = address;
 	}
-	
+
 }
