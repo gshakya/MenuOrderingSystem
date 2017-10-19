e3dfdb354dd8418be1dffd4c8a9eef58feaa5c78
Gunjan <gr.skya@gmail.com>
2017-10-19 13:56:53 -0500

    UserOperations Disabled

--- src/presentationLayer/Login.java
+++ src/presentationLayer/Login.java
@@ -59,7 +59,7 @@ public class Login {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		uo = new UsersOperation();
+//		uo = new UsersOperation();
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
